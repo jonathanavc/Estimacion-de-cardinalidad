e51f8a4d3993b51d1d5b46ba0aef15a25356d589
jonathanavc <jonvenega@gmail.com>
2022-09-15 21:18:16 -0400

    🐛

--- main.cpp
+++ main.cpp
@@ -5,23 +5,36 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
-    string fn_g1 = "GCF_000001405.39_GRCh38.p13_genomic.fna";
-    string fn_g2 = "GCF_000308155.1_EptFus1.0_genomic.fna";
+    if(argc != 5){
+        cout << "Modo de uso "<< argv[0] << "  n°bits_sketch  n°threads  archivo1 archivo2" <<endl;
+        return 1;
+    }
+    string fn_g1 = argv[4];
+    string fn_g2 = argv[3];
 
-    unsigned short k = 14; //N° de bits para buckets
+
+    unsigned short k = atoi(argv[1]); //N° de bits para sketch
     unsigned short k_mers = 31;
-    unsigned short n_threads = 8;
+    unsigned short n_threads = atoi(argv[2]);
 
-    hyperloglog g1(fn_g1, k, n_threads);
-    g1.calcular();
-    cout << g1.resultado() << endl;
+    hyperloglog g1_hll(fn_g1, k, n_threads);
+    pcsa g1_pcsa(fn_g1, k, n_threads);
+    hyperloglog g2_hll(fn_g2, k, n_threads, k_mers);
+    pcsa g2_pcsa(fn_g2, k, n_threads);
 
-    hyperloglog g2(fn_g2, k, n_threads, k_mers);
-    g2.calcular();
-    cout << g2.resultado() << endl;
+    g1_hll.calcular();
+    cout <<"("<< fn_g1 <<")hyperloglog: "<< g1_hll.resultado() << endl;
 
-    cout << _union(k, &g1, &g2) << endl;
-    cout << "jaccard: "<< _jaccard(k, &g1, &g2) <<endl;
+    g1_pcsa.calcular();
+    cout <<"("<< fn_g1 <<")pcsa: "<< g1_pcsa.resultado() << endl;
+
+    g2_hll.calcular();
+    cout <<"("<< fn_g2 <<")hyperloglog: "<< g1_hll.resultado() << endl;
+
+    g2_pcsa.calcular();
+    cout <<"("<< fn_g2 <<")pcsa: "<< g2_pcsa.resultado() << endl;
+
+    cout << "jaccard: "<< _jaccard(k, &g1_hll, &g2_hll) <<endl;
 
     return 0;
 }
